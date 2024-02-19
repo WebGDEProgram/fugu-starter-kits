992685461b8b851dc3a8bd8810851ab5ecd66d06
Christian Liebel <christian.liebel@thinktecture.com>
2024-02-19 12:43:25 +0100

    Add file handling library for Angular

---
+++ packages/fugu-angular/file-handling/README.md
@@ -0,0 +1,7 @@
+# @fugu-angular/file-handling
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test @fugu-angular/file-handling` to execute the unit tests.
---
+++ packages/fugu-angular/file-handling/src/lib/file-handling/file-handling.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-file-handling',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './file-handling.component.html',
+  styleUrl: './file-handling.component.css',
+})
+export class FileHandlingComponent {}
